f28a1d4bb4b4324804f3d5cedb50ecff71e0b951
victor <victorezeabikwa@gmail.com>
2024-04-26 13:11:06 +0100

    worked on sponsor section

--- components/ui/mobile-menu.tsx
+++ components/ui/mobile-menu.tsx
@@ -41,7 +41,7 @@ export default function MobileMenu() {
         onClick={() => setMobileNavOpen(!mobileNavOpen)}
       >
         <span className="sr-only">Menu</span>
-        <svg className="w-5 h-5 fill-current text-white hover:text-black transition duration-150 ease-in-out" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
+        <svg className="w-5 h-5 fill-current text-white  transition duration-150 ease-in-out" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
           <rect y="2" width="20" height="2" rx="1" />
           <rect y="9" width="20" height="2" rx="1" />
           <rect y="16" width="20" height="2" rx="1" />
@@ -57,17 +57,17 @@ export default function MobileMenu() {
       >
         <ul className="border border-transparent rounded-lg px-4 py-1.5 " >
           <li>
-            <Link className="flex font-medium text-sm text-white hover:text-black py-1.5" href="#">HOME </Link>
+            <Link className="flex font-medium text-sm text-white  py-1.5" href="#">HOME </Link>
           </li>
           <li>
-            <Link className="flex font-medium text-sm text-white hover:text-black py-1.5 mb-2 " href="#">ABOUT US</Link>
+            <Link className="flex font-medium text-sm text-white  py-1.5 mb-2 " href="#">ABOUT US</Link>
           </li>
          
           <li>
-            <Link className="flex font-medium text-sm text-white hover:text-black py-1.5" href="#">CONTACT US</Link>
+            <Link className="flex font-medium text-sm text-white py-1.5" href="#">CONTACT US</Link>
           </li>
           <li>
-            <Link className="flex font-medium text-sm text-white hover:text-black py-1.5 mb-2 " href="#">MUNATARE</Link>
+            <Link className="flex font-medium text-sm text-white  py-1.5 mb-2 " href="#">MUNATARE</Link>
           </li>
         </ul>
       </nav>
